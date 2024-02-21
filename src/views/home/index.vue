4a851951d070f6074108beec5d311fe17701ab03
WZ <2072401390@qq.com>
2024-02-21 11:51:09 +0800

    feat: 增加登录按钮

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -3,8 +3,15 @@
   <div>
     <h1>一级路由 home</h1>
   </div>
+  <el-button type="default" @click="click">登录</el-button>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+const router = useRouter()
+function click() {
+  router.push('/login')
+}
+</script>
 
 <style scoped></style>
